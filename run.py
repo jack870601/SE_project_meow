1802e11731e3da4448a0f8fa0c3e18eb0f4e1aca
milli <cal456093@gmail.com>
2018-12-31 10:37:10 +0800

    lottery+merge
    meow

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
-from  controller.home import home
-from  controller.store import store
-from  controller.login import login
+from controller.home import home
+from controller.store import store
+from controller.login import login
 #from views.Board import Board
 
 
