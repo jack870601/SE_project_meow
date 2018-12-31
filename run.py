ce82ad2c5059a942631b6e55c8d273a8fe9381d4
jack <jack870601@gmail.com>
2018-12-31 12:42:01 +0800

    update lottery URL & modify

--- run.py
+++ run.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 from  controller.home import home
-from  controller.store import store
 from  controller.login import login
 from  controller.title import title
+from  controller.lottery import lottery
 #from views.Board import Board
 
 
 app = Flask(__name__)
 
 app.register_blueprint(home)
-app.register_blueprint(store)
 app.register_blueprint(login)
 app.register_blueprint(title)
+app.register_blueprint(lottery)
 @app.route('/')
 def router():
     return render_template('index.html')
