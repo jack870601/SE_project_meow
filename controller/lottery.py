27a3420d023e9f6f96fee9a2128ab55f87b48cd6
jack <jack870601@gmail.com>
2019-01-04 04:41:54 +0800

    version3

--- controller/lottery.py
+++ controller/lottery.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template,Blueprint, request,session
-
+from model.User import User
 #from views.Board import Board
 
 
 lottery = Blueprint('lottery',__name__)
-@lottery.route('/lottery') #定義URL
+@lottery.route('/lottery', methods=['GET', 'POST']) #定義URL
 def router_lottery():
 	if session.get("user"):
 		user = session['user']
 		username = user['username']
+
+	if request.method =="POST":
+		NewUser_name = User.filter(id=1)[0]['username']
 	return render_template('/lottery.html', **locals())
 
