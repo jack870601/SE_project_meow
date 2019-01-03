3831c03f77a121c7bc483705c879241f57979555
jellyfishuang <dnc22412125@gmail.com>
2019-01-03 14:48:54 +0800

    increase person
    update

--- run.py
+++ run.py
@@ -4,6 +4,8 @@ from controller.store import store
 from controller.login import login
 from controller.title import title
 from controller.lottery import lottery
+from controller.person import person
+
 #from views.Board import Board
 
 
@@ -13,6 +15,8 @@ app.register_blueprint(home)
 app.register_blueprint(login)
 app.register_blueprint(title)
 app.register_blueprint(lottery)
+app.register_blueprint(person)
+
 @app.route('/')
 def router():
     return render_template('index.html')
