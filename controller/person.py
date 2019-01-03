06ead2c973755094b6faf20c7b018ad6ac0ff452
jellyfishuang <dnc22412125@gmail.com>
2019-01-03 15:52:59 +0800

    update
    update home,login,register,lottery,person,store,title

--- controller/person.py
+++ controller/person.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template,Blueprint, request
 #from views.Board import Board
 
 
-login = Blueprint('login',__name__)
-@login.route('/login') #定義URL
+person = Blueprint('person',__name__)
+@person.route('/person') #定義URL
 def router_login():
-    return render_template('/login.html')
+    return render_template('/person.html')
 
--- controller/store.py
+++ controller/store.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template,Blueprint, request
 #from views.Board import Board
 
 
-home = Blueprint('home',__name__)
-@home.route('/home') #定義URL
+store = Blueprint('store',__name__)
+@store.route('/store') #定義URL
 def router_home():
-    return render_template('/home.html')
+    return render_template('/store.html')
 
